0d493daa527d79841eea93d53ccde615f0101285
serv-inc <serv-inc@users.noreply.github.com>
2017-12-31 15:06:21 +0100

    sometimes failed to show the block set

--- addon/blockpage.js
+++ addon/blockpage.js
@@ -3,10 +3,10 @@
 /* globals document */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 
-var params = decodeURIComponent(document.location.search.slice(1)).split('&');
+var params = document.location.search.slice(1).split('&');
 // page
-document.querySelector("#page").innerText = params[0];
+document.querySelector("#page").innerText = decodeURIComponent(params[0]);
 // block set
 if (params[1] !== "-1") {
-  document.querySelector("#blockset").innerText = params[1];
+  document.querySelector("#blockset").innerText = decodeURIComponent(params[1]);
 }
