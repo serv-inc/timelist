867a59572cb48eda862292ad539065525f6dfe1c
serv-inc <serv-inc@users.noreply.github.com>
2019-05-22 19:04:59 +0200

    style: was uglier

--- addon/blockif.js
+++ addon/blockif.js
@@ -1,8 +1,6 @@
 "use strict";
 /* jshint esversion: 6, strict: global */
-/* globals chrome */
-/* globals location */
-/* globals document */
+/* globals chrome, document, location */
 // licensed under the MPL 2.0 by (github.com/serv-inc)
 
 /**
